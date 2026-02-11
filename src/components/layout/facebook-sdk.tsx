bd682e9d17c31084a4929a2b72c8178dcf650daa
Sourabh <46265339+Sourabh0777@users.noreply.github.com>
2026-02-11 17:11:11 +0530

    feat: Add Facebook SDK integration for login functionality and define its environment variables.

--- src/components/layout/facebook-sdk.tsx
+++ src/components/layout/facebook-sdk.tsx
@@ -2,36 +2,46 @@
 
 import Script from 'next/script';
 import { useEffect } from 'react';
+import { env } from '@/env';
 
 declare global {
   interface Window {
     fbAsyncInit: () => void;
     FB: any;
+    checkLoginState: () => void;
   }
 }
 
 export default function FacebookSDK() {
   const statusChangeCallback = (response: any) => {
-    console.log('FB login status response:', response);
-    // status => connected , not_authorized , unknown
-    console.log('Successfully logged in with Facebook', response.status);
-
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
-      // authResponse is included if the status is connected and is made up of the following:
-      // accessToken - contains an access token for the person using the app.
-      // expiresIn - indicates the UNIX time when the token expires and needs to be renewed.
-      // signedRequest - a signed parameter that contains information about the person using the app.
-      // userID - the ID of the person using the app.
-
-      console.log('Successfully logged in with Facebook', response.authResponse);
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      console.log('Logged into Facebook, but not authorized for this app');
+      testAPI();
     } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      console.log('Not logged into Facebook');
+      // The person is not logged into your app or we are unable to tell.
+      const statusElement = document.getElementById('status');
+      if (statusElement) {
+        statusElement.innerHTML = 'Please log into this app.';
+      }
+    }
+  };
+
+  const testAPI = () => {
+    console.log('Welcome!  Fetching your information.... ');
+    if (window.FB) {
+      window.FB.api('/me', function (response: any) {
+        console.log('Successful login for: ' + response.name);
+        const statusElement = document.getElementById('status');
+        if (statusElement) {
+          statusElement.innerHTML = 'Thanks for logging in, ' + response.name + '!';
+        }
+      });
     }
   };
 
@@ -39,14 +49,25 @@ export default function FacebookSDK() {
     window.fbAsyncInit = function () {
       if (window.FB) {
         window.FB.init({
-          appId: process.env.NEXT_PUBLIC_FACEBOOK_APP_ID || '{your-app-id}',
-          cookie: true,
-          xfbml: true,
-          version: process.env.NEXT_PUBLIC_FACEBOOK_API_VERSION || 'v18.0',
+          appId: env.NEXT_PUBLIC_FACEBOOK_APP_ID,
+          cookie: true, // enable cookies to allow the server to access
+          // the session
+          xfbml: true, // parse social plugins on this page
+          version: env.NEXT_PUBLIC_FACEBOOK_API_VERSION, // Specify the Graph API version to use
         });
 
-        window.FB.AppEvents.logPageView();
+        // Now that we've initialized the JavaScript SDK, we call
+        // FB.getLoginStatus().  This function gets the state of the
+        // person visiting this page and can return one of three states to
+        // the callback you provide.
+        window.FB.getLoginStatus(function (response: any) {
+          statusChangeCallback(response);
+        });
+      }
+    };
 
+    window.checkLoginState = function () {
+      if (window.FB) {
         window.FB.getLoginStatus(function (response: any) {
           statusChangeCallback(response);
         });
@@ -54,5 +75,23 @@ export default function FacebookSDK() {
     };
   }, []);
 
-  return <Script id="facebook-jssdk" src="https://connect.facebook.net/en_US/sdk.js" strategy="afterInteractive" />;
+  return (
+    <>
+      <Script id="facebook-jssdk" src="https://connect.facebook.net/en_US/sdk.js" strategy="afterInteractive" />
+      {env.NEXT_PUBLIC_FACEBOOK_CONFIG_ID && (
+        <div className="fixed bottom-4 right-4 z-[9999] bg-white p-4 rounded-lg shadow-xl border border-gray-200">
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `<fb:login-button 
+              scope="public_profile,email" 
+              config_id="${env.NEXT_PUBLIC_FACEBOOK_CONFIG_ID}"
+              onlogin="checkLoginState();">
+            </fb:login-button>`,
+            }}
+          />
+          <div id="status" className="mt-2 text-sm text-gray-600"></div>
+        </div>
+      )}
+    </>
+  );
 }
